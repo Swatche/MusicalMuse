5793b25295f886d6eb6a83940473934fa0c20985
Sammy Mak <samantha.mak@hotmail.com>
2024-02-11 21:48:33 -0500

    added the domReady function that unregistered the pill-shape style from social-links

--- plugins/musicalmuse-core-block-hooks/plugin.js
+++ plugins/musicalmuse-core-block-hooks/plugin.js
@@ -1,4 +1,9 @@
 wp.blocks.registerBlockStyle( 'core/paragraph', {
-    name: 'fancy-quote',
-    label: 'Fancy Quote',
+    name: 'fancy-paragraph',
+    label: 'Fancy Paragraph',
 } );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/social-links', 'pill-shape' );
+} );
+
