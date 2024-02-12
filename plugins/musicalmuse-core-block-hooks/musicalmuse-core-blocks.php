3c7f6b6494987e0fc5fe0437034372a3ed76e207
Sammy Mak <samantha.mak@hotmail.com>
2024-02-11 21:38:37 -0500

    added the enqueue function to enqueue plugin.js

--- plugins/musicalmuse-core-block-hooks/musicalmuse-core-blocks.php
+++ plugins/musicalmuse-core-block-hooks/musicalmuse-core-blocks.php
@@ -3,5 +3,15 @@
  * Plugin Name: MusicalMuse Core Block Hooks
  */
 
+function musicalmuse_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'musicalmuse-core-block-hooks',
+        plugins_url( 'plugin.js', __FILE__ ),
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( plugin_dir_path( __FILE__ ) . '/plugin.js' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'musicalmuse_enqueue_block_editor_assets' );
+
 
 
