a982bde268910efbf37408fc1206d2fa00c2dd50
Sammy Mak <samantha.mak@hotmail.com>
2024-02-12 22:31:05 -0500

    registered heading-2 block style

--- plugins/musicalmuse-core-block-hooks/plugin.js
+++ plugins/musicalmuse-core-block-hooks/plugin.js
@@ -3,7 +3,12 @@ wp.blocks.registerBlockStyle( 'core/paragraph', {
     label: 'Fancy Paragraph',
 } );
 
-wp.domReady( function () {
-    wp.blocks.unregisterBlockStyle( 'core/social-links', 'pill-shape' );
+wp.blocks.registerBlockStyle( 'core/heading', {
+    name: 'heading-2',
+    label: 'Heading 2',
 } );
+
+// wp.domReady( function () {
+//     wp.blocks.unregisterBlockStyle( 'core/social-links', 'pill-shape' );
+// } );
 
