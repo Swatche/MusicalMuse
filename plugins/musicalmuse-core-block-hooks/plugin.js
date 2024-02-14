23b0cb24b5d6e3fc97be3a639ff1233bd89d6a07
Sammy Mak <samantha.mak@hotmail.com>
2024-02-13 22:32:07 -0500

    registered a new block called fake-input

--- plugins/musicalmuse-core-block-hooks/plugin.js
+++ plugins/musicalmuse-core-block-hooks/plugin.js
@@ -3,6 +3,11 @@ wp.blocks.registerBlockStyle( 'core/paragraph', {
     label: 'Fancy Paragraph',
 } );
 
+wp.blocks.registerBlockStyle( 'core/paragraph', {
+    name: 'fake-input',
+    label: 'Fake Input',
+} );
+
 wp.blocks.registerBlockStyle( 'core/heading', {
     name: 'heading-2',
     label: 'Heading 2',
