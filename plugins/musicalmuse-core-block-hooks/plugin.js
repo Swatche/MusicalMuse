ee1b6f2f5ceac06fae30a4e8d7d2d7f474237842
Sammy Mak <samantha.mak@hotmail.com>
2024-02-14 20:26:42 -0500

    registered text blocks as plugins

--- plugins/musicalmuse-core-block-hooks/plugin.js
+++ plugins/musicalmuse-core-block-hooks/plugin.js
@@ -13,6 +13,51 @@ wp.blocks.registerBlockStyle( 'core/heading', {
     label: 'Heading 2',
 } );
 
+wp.blocks.registerBlockStyle( 'core/list', {
+    name: 'fancy-list',
+    label: 'Fancy List',
+} );
+
+wp.blocks.registerBlockStyle( 'core/preformatted', {
+    name: 'fancy-preformatted',
+    label: 'Fancy Preformatted',
+} );
+
+wp.blocks.registerBlockStyle( 'core/pullquote', {
+    name: 'fancy-pullquote',
+    label: 'Fancy Pullquote',
+} );
+
+wp.blocks.registerBlockStyle( 'core/table', {
+    name: 'fancy-table',
+    label: 'Fancy Table',
+} );
+
+wp.blocks.registerBlockStyle( 'core/verse', {
+    name: 'fancy-verse',
+    label: 'Fancy Verse',
+} );
+
+wp.blocks.registerBlockStyle( 'core/quote', {
+    name: 'fancy-quote',
+    label: 'Fancy Quote',
+} );
+
+wp.blocks.registerBlockStyle( 'core/footnotes', {
+    name: 'fancy-footnotes',
+    label: 'Fancy Footnotes',
+} );
+
+wp.blocks.registerBlockStyle( 'core/details', {
+    name: 'fancy-details',
+    label: 'Fancy Details',
+} );
+
+wp.blocks.registerBlockStyle( 'core/list', {
+    name: 'fancy-list',
+    label: 'Fancy List',
+} );
+
 // wp.domReady( function () {
 //     wp.blocks.unregisterBlockStyle( 'core/social-links', 'pill-shape' );
 // } );
