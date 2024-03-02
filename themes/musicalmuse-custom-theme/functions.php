0a968b247af24b41fa251776f03e459772a610b4
Sammy Mak <samantha.mak@hotmail.com>
2024-03-01 21:09:33 -0500

    enabled block editor for woocommerce

--- themes/musicalmuse-custom-theme/functions.php
+++ themes/musicalmuse-custom-theme/functions.php
@@ -24,3 +24,12 @@ function musicalmuse_wp_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'musicalmuse_wp_enqueue_scripts' );
+
+function block_theme_custom_use_block_editor_for_post_type($can_edit, $post_type){
+	if($post_type == 'product'){
+		$can_edit = true;
+	}
+	
+	return $can_edit;
+}
+add_filter('use_block_editor_for_post_type', 'block_theme_custom_use_block_editor_for_post_type', 10, 2);
